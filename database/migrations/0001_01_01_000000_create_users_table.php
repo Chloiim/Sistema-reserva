4c166c8fe29802ff1d4169cd00c09a469f7bebd5
Chloiim <styngersr@gmail.com>
2026-05-31 19:47:40 -0500

    Error solucionado en la tabla create users con un campos de string

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->strinng('ubicacion')->nullable();
+            $table->string('ubicacion')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
