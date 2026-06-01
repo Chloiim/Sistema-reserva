ca5bde7664a48dc2364873f3186e4e0bc1fcd68c
Chloiim <styngersr@gmail.com>
2026-06-01 00:32:36 -0500

    Se agrego mas campos a la models de User

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,10 @@ class User extends Authenticatable
         'email',
         'password',
         'tipo_usuario',
+        'ubicacion',
+        'direccion',
+        'telefono',
+        'descripcion',
     ];
 
     /**
